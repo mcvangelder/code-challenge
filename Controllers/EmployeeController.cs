a1d8f0c51a58761833684c84fc1d5ae81b4aa38b
Matthew Van Gelder <vangelder@gmail.com>
2018-03-20 21:50:41 -0700

    Functioning Employee API using .net core

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace challenge.Controllers
+{
+    [Route("api/employee")]
+    public class EmployeeController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(ILogger<EmployeeController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee([FromBody] Employee employee)
+        {
+            _logger.LogDebug($"Received employee create request for '{employee.FirstName} {employee.LastName}'");
+
+            _employeeService.Create(employee);
+
+            return CreatedAtRoute("getEmployeeById", new { id = employee.EmployeeId }, employee);
+        }
+
+        [HttpGet("{id}", Name = "getEmployeeById")]
+        public IActionResult GetEmployeeById(String id)
+        {
+            _logger.LogDebug($"Received employee get request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateEmployee(String id, [FromBody]JsonPatchDocument<Employee> employeePatch)
+        {
+            _logger.LogDebug($"Recieved employee update request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+                return NotFound();
+
+            employeePatch.ApplyTo(employee);
+
+            _employeeService.Update(employee);
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        public Employee Create(Employee employee)
+        {
+            if(employee != null)
+            {
+                _employeeRepository.Add(employee);
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return employee;
+        }
+
+        public Employee GetById(string id)
+        {
+            if(!String.IsNullOrEmpty(id))
+            {
+                return _employeeRepository.GetById(id);
+            }
+
+            return null;
+        }
+
+        public Employee Update(Employee employee)
+        {
+            if(employee != null)
+            {
+                _employeeRepository.Update(employee);
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return employee;
+        }
+    }
+}
